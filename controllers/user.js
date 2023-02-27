9706803d8880cb49ae241a367f668310ad673fc1
Sumit Khamaru <sumitkhmaru7@gmail.com>
2023-02-27 15:59:10 +0530

    "Auth fix"

--- controllers/user.js
+++ controllers/user.js
@@ -31,7 +31,7 @@ export const register = async (req, res) => {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
       sameSite: "none",
-      // secure: true
+      secure: true
     };
     res.status(201).cookie("token", token, options).json({
       success: true,
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
       sameSite: "none",
-      // secure: true,
+      secure: true,
     };
     res.cookie("token", token, options);
     return res.status(201).cookie("token", token, options).json({
@@ -103,7 +103,7 @@ export const logout = (req, res) => {
     expires: new Date(Date.now()),
     httpOnly: true,
     sameSite: "none",
-    // secure: true
+    secure: true
   };
   try {
     res.status(200).cookie("token", null, options).json({
